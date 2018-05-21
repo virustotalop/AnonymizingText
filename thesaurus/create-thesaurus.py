8d20081ec89afd83f07f5e637420ff24a63c8c7c
virustotalop <virustotalop@users.noreply.github.com>
2018-05-21 15:57:43 -0700

    Updated thesaurus to use wordnet
    Updated thesaurus to now using wordnet by openoffice

---
+++ thesaurus/create-thesaurus.py
@@ -0,0 +1,22 @@
+import json
+
+dict = {}
+
+key = None
+openedfile = open("thesaurus.dat", "r")
+lines = openedfile.readlines()
+for line in lines:
+    if "|" in line:
+        if key is None:
+            split = line.split('|')
+            key = split[0]
+        else:
+            split = line.split('|')
+            dict[key] = split[1].replace("\n", "")
+            key = None
+openedfile.close()
+json = json.dumps(dict)
+print(json)
+outfile = open("thesaurus.json", "w")
+outfile.write(json)
+outfile.close()
