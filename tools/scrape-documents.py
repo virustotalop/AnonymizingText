fd6636dd1be2370cd21b7b97309df8fdf9e6ecf6
virustotalop <virustotalop@users.noreply.github.com>
2018-06-04 20:17:08 -0700

    Finished scraping tool
    User details have to be passed by command line.

--- tools/scrape-documents.py
+++ tools/scrape-documents.py
@@ -1,5 +1,6 @@
 import praw
 import sys
+import os
 
 args = sys.argv
 len = len(args)
@@ -12,6 +13,13 @@ else:
 	password = args[3]
 	user_agent = args[4]
 	username = args[5]
-	
+	docs_dir = "docs"
+	original_dir = os.path.join(os.getcwd(), os.path.join(docs_dir, "original"))
+	print(original_dir)
 	reddit = praw.Reddit(client_id = client_id, client_secret = client_secret, password = password, user_agent = user_agent, username = username)
-	
+	index = 1
+	for comment in reddit.subreddit('all').comments(limit=300):
+		out_file = open(os.path.join(original_dir, str(index) + ".txt"), "w", encoding="utf-8")
+		out_file.write(comment.body)
+		out_file.close()
+		index += 1
